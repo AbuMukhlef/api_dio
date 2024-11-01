78b9aeb35ed4f24cdd49921568446368cbf2286f
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-11-01 10:14:44 +0300

    Create rick_morty.dart

---
+++ lib/data/model/rick_and _morty/rick_morty.dart
@@ -0,0 +1,13 @@
+class RickMorty {
+  late int id;
+  late String name;
+  late String species;
+  late String type;
+  late String gender;
+  late Map<String, dynamic> origin;
+  late Map<String, dynamic> location;
+  late String image;
+  late List<String> episode;
+  late String url;
+  late String created;
+}
